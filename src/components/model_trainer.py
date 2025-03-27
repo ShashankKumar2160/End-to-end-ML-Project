96b7c7ed06a7a40daf40269d2ffd4d75cc6c501b
ShashankKumar2160 <shashankkumar2160@gmail.com>
2025-03-28 02:14:45 +0530

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -27,7 +27,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
     
-    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+    def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info(f'Training started...')
             X_train,y_train,X_test,y_test = (

65af8eb8ba9fbad157b2d8bdac7e7dea6fa4f487
ShashankKumar2160 <shashankkumar2160@gmail.com>
2025-03-28 01:48:21 +0530

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,9 +46,30 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            
+
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
-        except:
-            pass 
+                                             models=models)
+            
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException(f'Model training failed. Best model score: {best_model_score}')
+            logging.info(f'Best found model on both training and test dataset')
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+        except Exception as e:
+            raise CustomException(e,sys)
